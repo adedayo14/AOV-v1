7fb80252adf40e36dc7f2ba34270a112aa02dfec
Adedayo <adedayo.a.alao@gmail.com>
2025-09-07 19:39:25 +0100

    Fix Vercel deployment: add proper Remix server handler and routing config

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// This is required for Remix to work on Vercel
+import { createRequestHandler } from "@remix-run/vercel";
+import * as build from "../build/server/index.js";
+
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
