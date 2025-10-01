dabbe6030aca3a1a8ea892407af14e75690dee0b
Adedayo <adedayo.a.alao@gmail.com>
2025-10-01 11:15:57 +0100

    feat: Update version identifiers

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,6 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 
-// Redirect any visit to the root (/) to the embedded app layout at /app
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  url.pathname = "/app";
-  return redirect(url.toString());
+export const loader = () => {
+  return redirect("/app");
 };
 
-export default function RedirectToAdmin() {
-  return null;
-}
